bb1d4b97804b9b35355df919affa5a364b838903
Synctom <services@synctom.com>
2025-07-12 05:39:30 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://diniiz-backend.onrender.com/",
+        changeOrigin: true,
+        secure: true,
+      },
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
 })
